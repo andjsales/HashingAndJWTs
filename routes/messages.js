e6bf44c64d7b0193e46c8fb11c9e19abe346f013
andrew.sales <andjsales@gmail.com>
2024-02-03 18:10:15 -0600

    fix routes

--- routes/messages.js
+++ routes/messages.js
@@ -1,3 +1,10 @@
+const Router = require("express").Router;
+const router = new Router();
+const User = require("../models/user");
+const { ensureLoggedIn } = require("../middleware/auth");
+const { Message } = require('./models/message');
+
+
 /** GET /:id - get detail of message.
  *
  * => {message: {id,
@@ -11,6 +18,17 @@
  *
  **/
 
+router.get("/:id", ensureLoggedIn, async function (req, res, next) {
+    try {
+        const message = await Message.get(req.params.id);
+        if (message.from_user.username !== req.user.username && message.to_user.username !== req.user.username) {
+            res.status(403).send("Access Denied.");
+        }
+        res.json({ message });
+    } catch (error) {
+        res.status(error.status || 500).send(error.message);
+    }
+});
 
 /** POST / - post message.
  *
@@ -19,6 +37,16 @@
  *
  **/
 
+router.post('/', ensureLoggedIn, async (req, res, next) => {
+    try {
+        const { to_username, body } = req.body;
+        const from_username = req.user.username;
+        const message = await Message.create({ from_username, to_username, body });
+        res.json({ message });
+    } catch (error) {
+        res.status(error.status || 500).send(error.message);
+    }
+});
 
 /** POST/:id/read - mark message as read:
  *
@@ -28,3 +56,21 @@
  *
  **/
 
+router.post('/:id/read', ensureLoggedIn, async (req, res) => {
+    try {
+        const id = req.params.id;
+        const message = await Message.get(id);
+
+        if (message.to_user.username !== req.user.username) {
+            return res.status(403).send("Access denied.");
+        }
+
+        const readMessage = await Message.markRead(id);
+        res.json({ readMessage });
+    } catch (error) {
+        res.status(error.status || 500).send(error.message);
+    }
+});
+
+
+module.exports = router;
